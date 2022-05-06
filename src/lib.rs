a23bcfcdf619768492ffabba5309252beec23dc3
Karthik <extrav@protonmail.com>
2022-05-05 22:55:06 -0400

    rica

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,32 @@
 #![allow(clippy::wildcard_imports)]
 
 use seed::{prelude::*, *};
-
+use wasm_bindgen::prelude::*;
+use web_sys::{Element, HtmlElement, HtmlInputElement, KeyboardEvent, HtmlBodyElement, EventTarget, Event};
+//use js-sys::prelude::*;
 // ------ ------
 //     Init
 // ------ ------
 
 // `init` describes what should happen when your app started.
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model { counter: 0 }
+    Model {
+	editable_right_ica: String::new(),
+        report_right_ica: String::from(""),
+    }
 }
+
+// const initial_ica: String = String::from("Right ICA was");
 
 // ------ ------
 //     Model
 // ------ ------
 
 // `Model` describes our app state.
+//#[derive(Clone)]
 struct Model {
-    counter: i32,
+	editable_right_ica: String,		
+    report_right_ica: String,
 }
 
 // ------ ------
@@ -28,16 +37,54 @@ struct Model {
 // ------ ------
 
 // (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 // `Msg` describes the different events you can modify state with.
 enum Msg {
-    Increment,
+    RICA,
+    RicaEdit(String),
+	RefreshRICA,
 }
 
 // `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+
     match msg {
-        Msg::Increment => model.counter += 1,
+	/////////////////////////////////////
+	Msg::RefreshRICA => {model.editable_right_ica = String::from("Babbbb");}
+
+	
+        /////////////////////////////////////
+        Msg::RicaEdit(editable_right_ica) => {
+            model.report_right_ica = editable_right_ica;
+        }
+        ///////////////////////////////////////
+        Msg::RICA => {
+//			let target = event.current_target().unwrap();
+//let content = target.dyn_ref::<web_sys::HtmlElement>().unwrap().text_content();
+		model.editable_right_ica = model.report_right_ica.to_string();
+
+
+            let initial_rica: String = String::from(
+                "The right internal carotid artery was selected and injected with cranial views
+The right internal carotid artery appears normal without any areas of irregularity or stenosis.
+The right middle cerebral artery appears normal without any areas of irregularity or stenosis.
+The right anterior cerebral artery appears normal without any areas of irregularity or stenosis. 
+",
+            );
+
+            if model.editable_right_ica.is_empty() {
+                model.editable_right_ica = initial_rica;
+		model.report_right_ica = String::from("The right internal carotid artery was selected and injected with cranial views
+The right internal carotid artery appears normal without any areas of irregularity or stenosis.
+The right middle cerebral artery appears normal without any areas of irregularity or stenosis.
+The right anterior cerebral artery appears normal without any areas of irregularity or stenosis.
+");
+            } else {
+		model.report_right_ica = String::new();
+               
+		 model.editable_right_ica = model.report_right_ica.to_string();
+            }
+        }
     }
 }
 
@@ -48,9 +95,26 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 // `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
     div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model.counter, ev(Ev::Click, |_| Msg::Increment),],
+        C!["columns"],
+        div![
+            C!["column"],
+            button![
+                C!["button", "is-primary"],
+                "RICA",
+                ev(Ev::Click,|_| Msg::RICA),
+            ]
+        ],
+        div![
+            C!["column"],
+            textarea![
+                C!["has-background-warning "],
+                input_ev(Ev::Change, Msg::RicaEdit),
+                attrs!{At::Value => &model.report_right_ica;},
+
+            ],
+        ],
+//	div![button![C!["button"], "Refresh", ev(Ev::Click,|_| Msg::RefreshRICA),]],
+        div![C!["column"], textarea![attrs!{At::Value => &model.report_right_ica;}]],
     ]
 }
 
